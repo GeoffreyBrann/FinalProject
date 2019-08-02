9950ef783332249ab473839da56a67316af5660e
KyleNeputy <krn33@cornell.edu>
2019-08-02 17:55:18 -0400

    Merge branch 'master' of C:\Users\kylen\OneDrive\Documents\GitHub\FinalProject with conflicts.

--- venv/test.py
+++ venv/test.py
@@ -1,12 +1,5 @@
-from classes.person_class import Person
-
-from classes.country_class import Country
-
-def file(self):
-    open("people_info.txt", "w")
-
-
-
-
-
-jack = Person("Jack", "A")
+def show_results():
+    '''Displays the number of turns the user took and number of people infected out of total people'''
+    turns = str(int(number_of_turns + "taken! "))
+    number_people_infected = str(int(len(infected_people)) + "of " + int(len(total_people)) + "total")
+    return turns, number_people_infected
