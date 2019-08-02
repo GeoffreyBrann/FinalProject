203f6daedc2449fbd9c9d59eaa7e27667841788c
KyleNeputy <krn33@cornell.edu>
2019-08-02 18:01:16 -0400

    Update test.py

--- venv/test.py
+++ venv/test.py
@@ -1,5 +1,7 @@
 def show_results():
     '''Displays the number of turns the user took and number of people infected out of total people'''
-    turns = str(int(number_of_turns + "taken! "))
-    number_people_infected = str(int(len(infected_people)) + "of " + int(len(total_people)) + "total")
-    return turns, number_people_infected
+    turns = int(number_of_turns)
+    number_people_infected = int(len(infected_people))
+    total_people = int(len(all_people))
+    print("You took " + turns)
+    print("You infected " + number_people_infected + " out of " + total_people)
