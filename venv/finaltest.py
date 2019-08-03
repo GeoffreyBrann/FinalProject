4818695128473a6602b4f7968c3c1b9f5b365295
KyleNeputy <krn33@cornell.edu>
2019-08-03 13:43:03 -0400

    finalproject

--- venv/finaltest.py
+++ venv/finaltest.py
@@ -28,6 +28,14 @@ def midGame(map):
             map.infect_mutual_friends(person)
         print("Press enter to take a turn: ")
 
+def show_results():
+    '''Displays the number of turns the user took and number of people infected out of total people'''
+    turns = int(number_of_turns)  ###number_of_turns is made up
+    number_people_infected = int(len(infected_people))  ###infected_people made up right now
+    total_people = int(len(all_people))   ####all_people made up for now
+    print("You took " + str(turns))
+    print("You infected " + str(number_people_infected) + " out of " + str(total_people))
+
 
 if __name__ == "__main__":
     michelle = Person("Michelle", "a")  ###creates a group of people
