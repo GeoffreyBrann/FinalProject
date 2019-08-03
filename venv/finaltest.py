113dd038247bf875bcb4659a2e35d71ef864e45a
KyleNeputy <krn33@cornell.edu>
2019-08-03 13:48:53 -0400

    finalproject

--- venv/finaltest.py
+++ venv/finaltest.py
@@ -29,11 +29,10 @@ def mid_game(our_map):
             our_map.infect_mutual_friends(person)
         print("Press enter to take a turn: ")
 
-def show_results():
-    '''Displays the number of turns the user took and number of people infected out of total people'''
+    '''Display number of turns player took and how many people they infected'''
     turns = int(number_of_turns)  ###number_of_turns is made up
-    number_people_infected = int(len(infected_people))  ###infected_people made up right now
-    total_people = int(len(all_people))   ####all_people made up for now
+    number_people_infected = int(len(our_map.infected_people))  ###infected_people made up right now
+    total_people = int(len(all_people))  ####all_people made up for now
     print("You took " + str(turns))
     print("You infected " + str(number_people_infected) + " out of " + str(total_people))
 
