b6805dbddeb28245424512b64cc87b9fc250ccef
Geoff <gwb73@cornell.edu>
2019-08-03 15:53:39 -0400

    active_infect

--- venv/classes/map_class.py
+++ venv/classes/map_class.py
@@ -46,6 +46,13 @@ class Map:
         for i in temp:
             self.infected_people.append(i)
 
+    def active_infect(self):
+        self.print_not_infected_people()
+        i = input("Enter the name of the person you want to infect")
+        person = self.find_person_by_name(i)
+        person.infected = True
+        self.infected_people.append(person)
+
     def print_people(self):
         for i in self.get_countries():
             for j in i.citizens:
